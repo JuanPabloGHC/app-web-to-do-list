9b1fff8a3057b828807efb0e40725281389da26c
JuanPabloGHC <jpghc552@hotmail.com>
2023-03-22 10:52:07 -0600

    bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, render_template, redirect, abort
 import requests
+#import os
+
+#os.environ['NO_PROXY'] = '127.0.0.1'
 
 app = Flask(__name__)
 
 url = 'https://to-do-list-api-ldkz.onrender.com/api/tasks'
+#url = 'http://127.0.0.1:5000/api/tasks'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
